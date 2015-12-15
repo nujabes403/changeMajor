2465bae982dffb9e1989fecaf812972cdc5b0bef
nujbaes403 <nujabes403@gmail.com>
2015-12-15 15:00:15 +0900

    fixed average point 15.12.15

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -32,6 +32,10 @@ module.exports = React.createClass({
   },
   calculate:function(){
     var point_B_conversion = 0;
+    if(this.state.point_A > 4.5 || this.state.point_A <= 0){
+      alert("평점평균을 올바르게 입력해주세요.");
+      return;
+    }
     if(this.state.want_major == null){
       alert("희망학과를 입력해주세요. \n\n ex)경영학과 or 경영 / 컴퓨터공학과 or 컴공");
       return;
@@ -39,7 +43,12 @@ module.exports = React.createClass({
     if(this.state.point_B < 33){
       alert("취득학점 33점 미만은 계산할 수 없습니다.");
       return;
-    } else if (this.state.point_B == 33){
+    }
+    else if(this.state.point_B > 50){
+      alert("취득학점 40이상은 모두 같은 환산점수로 반영됩니다.");
+      return;
+    }
+      else if (this.state.point_B == 33){
       point_B_conversion = 3.8;
     } else if (this.state.point_B == 34){
       point_B_conversion = 3.9;
@@ -56,7 +65,6 @@ module.exports = React.createClass({
     } else if (this.state.point_B >= 40){
       point_B_conversion = 4.5;
     }
-
     if(this.state.point_A == null && this.state.point_B == null){
       alert("점수를 입력해주세요.");
     } else if(this.state.point_A != null && point_B_conversion !=0 ){
