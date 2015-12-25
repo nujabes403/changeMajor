e83eaf1626b5883988d9c4084b433dbd7147b31b
nujbaes403 <nujabes403@gmail.com>
2015-12-25 12:17:52 +0900

    google adsense react ver

--- src/layout.jsx
+++ src/layout.jsx
@@ -5,13 +5,16 @@ var Layout_header = require('./layout_header');
 var Footer = require('./footer');
 var Calculator = require('./calculator');
 var Chat = require('./chat');
+var GoogleAd = require('./googleAd');
+
 module.exports = React.createClass({
   render:function(){
     return <div className="container-fluid layout2_main">
       {this.props.children ?
         this.props.children : <div><Layout_header/><Calculator/></div>}
         <Chat/>
-      <Footer/>
+        <GoogleAd client="ca-pub-9897177631447876" slot="2272212544" format="rectangle"/>
+    <Footer/>
     </div>
   }
 });
